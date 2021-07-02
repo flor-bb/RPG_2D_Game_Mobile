dd49ba5d4e08bd06c20505634924f747b463610e
Florian Bruderer <florian.bruderer@gmail.com>
2021-07-02 16:27:05 +0200

    Added a Camera which follows the player

---
+++ Assets/Scripts/SceneHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneHandler : MonoBehaviour
+{
+
+    public void ChangeToGameScene()
+    {
+
+        SceneManager.LoadScene(2);
+    }
+
+
+    public void QuitGame()
+    {
+
+        Application.Quit();
+
+    }
+
+}
